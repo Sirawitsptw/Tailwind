eaad71d5fe24fd9fd25ea74e61dc7d294599f084
Sirawitsptw <152370451+Sirawitsptw@users.noreply.github.com>
2025-03-22 12:23:07 +0700

    kkkk

--- tailwind-css/tailwind.config.js
+++ tailwind-css/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing : {
+        '72' : '18rem',
+      },
+      colors : {
+        teal : {
+          100 : "#E6FFFA"
+        }
+      }
+    },
   },
   plugins: [],
 };
